77d5cec83b86590a09b1c002244e7abc07beb6f3
evrahnos <evrahnos@chapman.edu>
2022-09-27 23:10:21 -0700

    Successful testing for function addP

---
+++ Assignment1/Part1/arithmetic.hs
@@ -0,0 +1,77 @@
+-- A Virtual Machine (VM) for Arithmetic
+-- Copyright: Alexander Kurz and Eleas Vrahnos 2022
+
+-----------------------
+-- Data types of the VM
+-----------------------
+
+-- Natural numbers
+data NN = O | S NN
+  deriving (Eq,Show) -- for equality and printing
+
+-- Integers
+data II = II NN NN
+  deriving Show -- for printing
+
+-- Positive integers (to avoid dividing by 0)
+data PP = I | T PP
+  deriving Show
+
+-- Rational numbers
+data QQ =  QQ II PP
+
+------------------------
+-- Arithmetic on the  VM
+------------------------
+
+----------------
+-- PP Arithmetic
+----------------
+
+-- add positive numbers
+addP :: PP -> PP -> PP
+addP I m = T m
+addP (T n) m = T (addP n m)
+
+----------------
+-- NN Arithmetic
+----------------
+
+-- add natural numbers
+addN :: NN -> NN -> NN
+addN O m = m
+addN (S n) m = S (addN n m)
+
+-- multiply natural numbers
+multN :: NN -> NN -> NN
+multN O m = O
+multN (S n) m = addN (multN n m) m
+
+----------------
+-- II Arithmetic
+----------------
+
+
+----------------
+-- QQ Arithmetic
+----------------
+
+
+----------------
+-- Normalisation
+----------------
+
+
+----------------------------------------------------
+-- Converting between VM-numbers and Haskell-numbers
+----------------------------------------------------
+
+
+----------
+-- Testing
+----------
+main = do
+    print $ addP (I) (T I) -- T I
+
+
+
