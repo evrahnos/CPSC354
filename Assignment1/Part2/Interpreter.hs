a8e2ae545c1d6ceee754d8792b9ee265958465ec
evrahnos <evrahnos@chapman.edu>
2022-09-30 15:44:32 -0700

    Successful testing for case Modulo in Interpreter.hs

--- Assignment1/Part2/Interpreter.hs
+++ Assignment1/Part2/Interpreter.hs
@@ -9,4 +9,5 @@ eval (Times n m) = (eval n) * (eval m)
 eval (Subtract n m) = (eval n) - (eval m) -- 6-3*2 == 0
 eval (Negative n) = - (eval n) -- -3*-4 == 12
 eval (Divide n m) = (eval n) `div` (eval m) -- 15/4 == 3
-eval (Exponent n m) = (eval n) ^ (eval m) -- 2^4 == 16
+eval (Exponent n m) = (eval n) ^ (eval m) -- 2^4/3 == 5
+eval (Modulo n m) = (eval n) `mod` (eval m) -- 12%5 == 2
