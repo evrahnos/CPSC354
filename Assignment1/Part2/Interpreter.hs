17b3e00925d9c05dd411f15a3d349fc0d949e2e5
evrahnos <evrahnos@chapman.edu>
2022-09-30 15:11:59 -0700

    Successful testing for case Negative in Interpreter.hs

--- Assignment1/Part2/Interpreter.hs
+++ Assignment1/Part2/Interpreter.hs
@@ -7,3 +7,4 @@ eval (Num n) = n
 eval (Plus n m) = (eval n) + (eval m)
 eval (Times n m) = (eval n) * (eval m)
 eval (Subtract n m) = (eval n) - (eval m) -- 6-3*2 == 0
+eval (Negative n) = - (eval n) -- -3*-4 == 12
