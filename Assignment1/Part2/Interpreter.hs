26de4ce27e391fce0c0c0956e2d8e2bf586bd181
evrahnos <evrahnos@chapman.edu>
2022-09-30 15:08:48 -0700

    Successful testing for case Subtract in Interpreter.hs

---
+++ Assignment1/Part2/Interpreter.hs
@@ -0,0 +1,9 @@
+module Interpreter where
+
+import AbsNumbers
+
+eval :: Exp -> Integer
+eval (Num n) = n
+eval (Plus n m) = (eval n) + (eval m)
+eval (Times n m) = (eval n) * (eval m)
+eval (Subtract n m) = (eval n) - (eval m) -- 6-3*2 == 0
